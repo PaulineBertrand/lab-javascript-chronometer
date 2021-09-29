0bfe620bc0375f4622728ef6c042ccc21dc63d64
Pauline Bertrand <bertrand.pauline@gmail.com>
2021-09-29 18:36:33 +0200

    some reorganizing made it work but there is no bonus

--- javascript/index.js
+++ javascript/index.js
@@ -13,53 +13,100 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
+// set an intervalId globally for use by multiple functions
+let intervalId;
+
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  let beautifulAlreadyFormattedTime = chronometer.split(); // why do I have to re-split this :'(
+  minDecElement.textContent = beautifulAlreadyFormattedTime[0];
+  minUniElement.textContent = beautifulAlreadyFormattedTime[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
-}
+  let beautifulAlreadyFormattedTime = chronometer.split(); 
+  secDecElement.textContent = beautifulAlreadyFormattedTime[3];
+  secUniElement.textContent = beautifulAlreadyFormattedTime[4];
+} 
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
 }
 
+function eraseClock() {
+  minDecElement.textContent = "0";
+  minUniElement.textContent = "0";
+  secDecElement.textContent = "0";
+  secUniElement.textContent = "0";
+}
+
 function printSplit() {
-  // ... your code goes here
+  let newSplitTime = document.createElement('li');
+  let splitPrintLocation = document.querySelector("#splits");
+  newSplitTime.textContent = chronometer.split();
+  splitPrintLocation.appendChild(newSplitTime);
+ 
 }
 
 function clearSplits() {
-  // ... your code goes here
+  let splitPrintLocation = document.querySelector("#splits");
+  splitPrintLocation.innerHTML = "";
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  btnLeftElement.classList.replace('stop', 'start');
+  btnLeftElement.textContent = 'START';
+  btnRightElement.classList.replace('split', 'reset');
+  btnRightElement.textContent = 'RESET';
+
+  chronometer.stop();
+  clearInterval(intervalId);
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  printSplit();
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnRightElement.textContent = 'SPLIT';
+  btnLeftElement.classList.replace('start', 'stop');
+
+  btnRightElement.classList.replace('reset', 'split');
+  btnLeftElement.textContent = 'STOP';
+  intervalId = setInterval(printTime,4);
+  chronometer.start();
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  clearSplits();
+  eraseClock();
+  chronometer.reset();
 }
 
 // Start/Stop Button
+
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+
+  if (btnLeftElement.classList.contains('start')) {
+    setStartBtn();
+
+  } else if (btnLeftElement.classList.contains('stop')) {
+      setStopBtn();
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+
+  if (btnRightElement.classList.contains('split')) {
+    setSplitBtn();
+  } else if (btnRightElement.classList.contains('reset')) {
+    setResetBtn();
+  }
 });
+
